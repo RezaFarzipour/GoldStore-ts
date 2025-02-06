09ff2ae761309a5fdf1dc630afbf4fe63f8f988e
RezaFarzipour <rezafafa78@gmail.com>
2025-02-06 13:25:18 +0330

    Initial commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import {yekan} from '../utils/font'
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="fa" dir="rtl">
       <Head />
-      <body>
+      <body className={yekan.className}>
         <Main />
         <NextScript />
       </body>
